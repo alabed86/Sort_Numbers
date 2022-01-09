59313a8be54fd6fda67d7c7ac89b1fae2ae42829
alabed86 <alabed@outlook.com>
2022-01-09 19:15:34 +0400

    update outputs

--- src/Sort/Sorter.java
+++ src/Sort/Sorter.java
@@ -1,32 +1,32 @@
 package src.Sort;
 public class Sorter {
 
-    public static int[] sort(int[] unsorted) {
+    public static int[] sort(int[] unsorted_array) {
 
-        int[] sorted = new int[unsorted.length];
-        sorted[0] = unsorted[0];
+        int[] sorted = new int[unsorted_array.length];
+        sorted[0] = unsorted_array[0];
 
-        for(int i = 1; i < unsorted.length; i++) {
-            if(unsorted[i] < sorted[i-1]) {
+        for(int i = 1; i < unsorted_array.length; i++) {
+            if(unsorted_array[i] < sorted[i-1]) {
                 for(int j = i-1; j >= 0; j--) {
-                    if(unsorted[i] < sorted[j]) {
+                    if(unsorted_array[i] < sorted[j]) {
                         sorted[j+1] = sorted[j];
-                        sorted[j] = unsorted[i];
+                        sorted[j] = unsorted_array[i];
                     } else {
                         break;
                     }
                 }
             } else {
-                sorted[i] = unsorted[i];
+                sorted[i] = unsorted_array[i];
             }
         }
 
         System.out.println("-------------------------------------------------");
-        System.out.println("Innan sortera: ");
-        for (int i = 0; i < unsorted.length; i++) {
-            System.out.print(unsorted[i] + ", ");
+        System.out.println("Befor sort numbers : ");
+        for (int i = 0; i < unsorted_array.length; i++) {
+            System.out.print(unsorted_array[i] + ", ");
         }
-        System.out.println("Efter sortera: ");
+        System.out.println("After sorting: ");
         for (int i = 0; i < sorted.length; i++) {
             System.out.print(sorted[i] + ", ");
         }
